94b917c276a8d63e5a9fe5b1a15f0f1d83d88f65
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-11-06 17:45:42 +0200

    Update c2232000022.lua

--- script/c2232000022.lua
+++ script/c2232000022.lua
@@ -35,8 +35,8 @@ function s.flipop2(e,tp,eg,ep,ev,re,r,rp)
 		and Duel.IsExistingMatchingCard(s.high_level_filter, tp, LOCATION_MZONE, 0, 1, nil)
 		and Duel.GetLocationCount(tp,LOCATION_SZONE)>0
 
-	local op=Duel.SelectEffect(tp,{b1,aux.Stringid(id,0)},
-								  {b2,aux.Stringid(id,1)})
+	local op=Duel.SelectEffect(tp, {b1,aux.Stringid(id,0)},
+								   {b2,aux.Stringid(id,1)})
 	op=op-1 --SelectEffect returns indexes starting at 1, so we decrease the result by 1 to match your "if"s
 
 	if op==0 then
