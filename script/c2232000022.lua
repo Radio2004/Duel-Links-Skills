3058321b313f176f767439ad1225e922234f11b4
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-11-06 17:55:38 +0200

    Update c2232000022.lua

--- script/c2232000022.lua
+++ script/c2232000022.lua
@@ -2,9 +2,23 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate Skill
-	aux.AddSkillProcedure(c,0,false,nil,nil)
-	aux.AddSkillProcedure(c,0,false,s.flipcon2,s.flipop2)
+	aux.AddSkillProcedure(c,2,false,nil,nil)
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetLabel(0)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1)
+	aux.AddSkillProcedure(c,2,false,s.flipcon2,s.flipop2)
 end
+
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	e:SetLabel(1)
+end
+
 
 function s.sendToGrave(c)
 	return c:IsAbleToGraveAsCost()
