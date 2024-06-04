7c21613c8aa4a323709cb084a320637f7e1e77a7
Andriy Shupa <andrijsupa@gmail.com>
2024-06-04 21:28:47 +0300

    Update c2232000022.lua

--- script/c2232000022.lua
+++ script/c2232000022.lua
@@ -22,9 +22,8 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetLabel()==0 then
 
 		local g=Duel.GetFieldGroup(tp,LOCATION_DECK,0)
-		Debug.Message(#g)
 		local onlyMonsterTimelords = g:Filter(s.filterTimelords,nil)
-		
+		Debug.Message(#onlyMonsterTimelords)
 		local con = Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_DECK, 0,1,nil, 36894320) and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_DECK, 0,1,nil,72883039) and (g:IsExists(Card.IsSetCard,1,nil,0x4a) or g:IsExists(Card.IsCode,1,nil,27107590))
 		if not con then
 			Duel.RegisterFlagEffect(tp,id,0,0,0)
