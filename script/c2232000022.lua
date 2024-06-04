f95dc897c58cc37e5acf4fb8b3e133747409cfba
Andriy Shupa <andrijsupa@gmail.com>
2024-06-04 17:17:30 +0300

    Update c2232000022.lua

--- script/c2232000022.lua
+++ script/c2232000022.lua
@@ -18,8 +18,7 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetLabel()==0 then
 
 		local g=Duel.GetDecktopGroup(tp,math.floor(Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)/2))
-		local con = Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_DECK, 0,1,nil, 36894320) and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_DECK, 0,1,nil,72883039) and (g:IsExists(Card.IsSetCard,1,nil,0x4a) or g:IsExists(Card.IsCode,1,nil,27107590))
-		Debug.Message(g)
+		local con = Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_DECK, 0,1,nil, 36894320) and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_DECK, 0,1,nil,72883039)
 		if not con then
 			Duel.RegisterFlagEffect(tp,id,0,0,0)
 			Duel.RegisterFlagEffect(tp,id+1,0,0,0)
