5643db1b2d3c35ec4bf9feb0e93819b344baa48a
Andriy Shupa <andrijsupa@gmail.com>
2024-06-06 21:59:20 +0300

    Update c2232000029.lua

--- script/c2232000029.lua
+++ script/c2232000029.lua
@@ -19,7 +19,7 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_CARD,tp,id)
 	local c=e:GetHandler()
 	if e:GetLabel()==0 then
-		local e1=Effect.CreateEffect(e:GetHandler())
+		local e1=Effect.CreateEffect(c)
 		e1:SetDescription(aux.Stringid(id,2))
 		e1:SetType(EFFECT_TYPE_FIELD)
 		e1:SetCode(EFFECT_SUMMON_PROC)
