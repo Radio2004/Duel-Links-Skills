a712360a3938bf3790fe0c164038a33b9e9862b9
Andriy Shupa <andrijsupa@gmail.com>
2024-06-06 21:01:53 +0300

    Update c2232000025.lua

--- script/c2232000025.lua
+++ script/c2232000025.lua
@@ -21,7 +21,7 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if e:GetLabel()==0 then
 		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 		e1:SetCondition(s.spcon)
 		e1:SetOperation(s.spop)
