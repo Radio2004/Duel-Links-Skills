14a5743d0aae272d37c9f73ba97e098076c0e30c
Andriy Shupa <andrijsupa@gmail.com>
2024-06-06 11:28:07 +0300

    Update c2232000025.lua

--- script/c2232000025.lua
+++ script/c2232000025.lua
@@ -90,7 +90,7 @@ function s.operation_for_res0(e,tp,eg,ep,ev,re,r,rp)
 		for i=1,#keyMonster do
 			local g=Duel.CreateToken(tp,keyMonster[i])
 			  Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
-			  c:SetCardTarget(g)
+			  e:GetHandler():SetCardTarget(g)
 			  Duel.SpecialSummonComplete()
 		end
 	end
