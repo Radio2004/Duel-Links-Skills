377acfb90b0b921aebd3131ca2ccacc4ad83a43a
Andriy Shupa <andrijsupa@gmail.com>
2024-06-06 11:27:20 +0300

    Update c2232000025.lua

--- script/c2232000025.lua
+++ script/c2232000025.lua
@@ -90,6 +90,8 @@ function s.operation_for_res0(e,tp,eg,ep,ev,re,r,rp)
 		for i=1,#keyMonster do
 			local g=Duel.CreateToken(tp,keyMonster[i])
 			  Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+			  c:SetCardTarget(g)
+			  Duel.SpecialSummonComplete()
 		end
 	end
 	Duel.RegisterFlagEffect(tp,id,0,0,0)
