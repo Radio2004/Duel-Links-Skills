43d91df35b4c5edbacff0312ee1898b16b8b3be0
Andriy Shupa <andrijsupa@gmail.com>
2024-06-08 17:50:12 +0300

    Update c2232000029.lua

--- script/c2232000029.lua
+++ script/c2232000029.lua
@@ -127,8 +127,22 @@ function s.operation_for_res0(e,tp,eg,ep,ev,re,r,rp)
 			end
 		end
 	end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_CLIENT_HINT+EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(s.splimit2)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
 	Duel.RegisterFlagEffect(tp,id,0,0,0)
 end
+
+function s.splimit2(e,c,sump,sumtype,sumpos,targetp,se)
+	return not c:IsLinkMonster() and c:IsLocation(LOCATION_EXTRA)
+end
+
 
 function s.operation_for_res1(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
